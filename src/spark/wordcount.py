c4f5e3941ce4e2010d0325434845308787d4119a
gchoy <gchoy@users.noreply.github.com>
2015-06-16 10:50:11 -0700

    changes to wordcount.py

--- src/spark/wordcount.py
+++ src/spark/wordcount.py
@@ -11,21 +11,15 @@ conf = SparkConf()
 conf.setMaster("local")
 conf.setAppName("WordCount")
 conf.set("spark.executor.memory", "1g")
-#sc = SparkContext(conf = conf)
 
 
 if __name__ == "__main__":
-    #if len(sys.argv) != 3:
-    #    print >> sys.stderr, "Usage: "
-    #    exit(-1)
-
-
+    
     sc = SparkContext(appName="WordCount")
     ssc = StreamingContext(sc, 2)
-    #zkQuorum = ("public-dns:2181","public-dns:2181","public-dns:2181","public-dns:2181")
+    #zkQuorum = 0"public-dns:2181","public-dns:2181","public-dns:2181","public-dns:2181")
     zkQuorum = "public dns:2181"
 
-    #brokers, topic = sys.argv[1:]
     kvs = KafkaUtils.createStream(ssc,zkQuorum,"consumer-random",{"random_words": 1} )
     lines = kvs.map(lambda x: x[1])
     counts = lines.flatMap(lambda line: line.split(" ")) \
