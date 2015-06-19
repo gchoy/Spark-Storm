9dc182648dee6657fee212459c3d45860319132f
gchoy <gchoy@users.noreply.github.com>
2015-06-19 10:36:40 -0700

    Changes to wordcount.py and producer.py

--- src/spark/producer.py
+++ src/spark/producer.py
@@ -15,14 +15,15 @@ class wordProducer(object):
     def random_words(self, source_symbol):
        wordlst = ["books","apple", "car","dog","cat","fish"]
        ln_cnt = 0
-       str_fmt ="{};{}"
+       #str_fmt ="{};{}"
        #rndword = random.choice(wordlst) + "," + random.choice(wordlst) + "," + random.choice(wordlst)
 
        while True:
            rndword = random.choice(wordlst) + "," + random.choice(wordlst) + "," + random.choice(wordlst)
-           msg_info = str_fmt.format(source_symbol,rndword)
+           #msg_info = str_fmt.format(source_symbol,rndword)
+           msg_info = rndword
            #print msg_info
-           sys.stdout.flush()
+           #sys.stdout.flush()
            self.producer.send_messages("random_words", source_symbol, msg_info)
            ln_cnt += 1
 
--- src/spark/wordcount.py
+++ src/spark/wordcount.py
@@ -17,12 +17,12 @@ if __name__ == "__main__":
     
     sc = SparkContext(appName="WordCount")
     ssc = StreamingContext(sc, 2)
-    #zkQuorum = 0"public-dns:2181","public-dns:2181","public-dns:2181","public-dns:2181")
-    zkQuorum = "public dns:2181"
+    #zkQuorum = "public-ip:2181, public-ip:2181, public-ip:2181, public-ip:2181"
+    zkQuorum = "public-ip:2181"
 
     kvs = KafkaUtils.createStream(ssc,zkQuorum,"consumer-random",{"random_words": 1} )
     lines = kvs.map(lambda x: x[1])
-    counts = lines.flatMap(lambda line: line.split(" ")) \
+    counts = lines.flatMap(lambda line: line.split(",")) \
         .map(lambda word: (word, 1)) \
         .reduceByKey(lambda a, b: a+b)
     counts.pprint()
